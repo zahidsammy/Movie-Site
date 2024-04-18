2d2185964d73bf4960631fccca20732d20d22c58
zahidsammy <zahidsammy95@gmail.com>
2024-04-18 01:40:57 -0700

    created the search bar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    
+
+    <div className='Search-bar-container' >
+      <div>Search Bar</div>
+      <div>Search results</div>
+    </div>
+  )
+}
+
+export default App
